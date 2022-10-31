991cdf2e1df3781050cd0c30b8563d96932ed352
Temp-user4 <rs20jan1998s@gmail.com>
2022-10-31 17:58:14 +0530

    init

--- app/src/main/java/com/example/uber_clone/GetDetails.java
+++ app/src/main/java/com/example/uber_clone/GetDetails.java
@@ -2,6 +2,7 @@ package com.example.uber_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,61 +10,43 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthRegistrar;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.FileReader;
 
 public class GetDetails extends AppCompatActivity {
 
-    EditText nameEt;
-    RadioGroup genderRg;
-    RadioButton maleRb,femaleRb,notToSayRb;
+    EditText nameEt, lastNameEt;
     Button submit;
-    String userGender;
-    FirebaseAuth userDetails;
+    FirebaseFirestore userDetails;
+    SharedPreferences sp;
+    String mobile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_details);
         nameEt=findViewById(R.id.nameET);
-        genderRg=findViewById(R.id.genderRg);
-        maleRb=findViewById(R.id.maleRb);
-        femaleRb=findViewById(R.id.femaleRb);
-        notToSayRb=findViewById(R.id.notToSayRb);
+        lastNameEt=findViewById(R.id.lastNameET);
         submit=findViewById(R.id.submitDetails);
-
-        genderRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int gender) {
-                switch (gender){
-                    case R.id.maleRb:{
-                        userGender="Male";
-                        break;
-                    }
-                    case R.id.femaleRb:{
-                        userGender="Female";
-                        break;
-                    }
-                    case R.id.notToSayRb:{
-                        userGender="Prefer Not To Disclose";
-                        break;
-                    }
-                }
-            }
-        });
+        userDetails=FirebaseFirestore.getInstance();
+        sp=getSharedPreferences("MyMobile",MODE_PRIVATE);
+        mobile=sp.getString("mobile","no");
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(nameEt.length()==0){
 
-                }else if(userGender==null){
+                }else if(lastNameEt.length()==0){
 
                 }else{
-                    System.out.println("qwerty"+nameEt.getText().toString()+"\t"+userGender);
-
+                    System.out.println("qwerty"+nameEt.getText().toString()+"\t"+lastNameEt.getText().toString());
+                    //fireStore need to
                 }
             }
         });
